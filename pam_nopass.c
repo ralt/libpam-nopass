374e0e1dda6b06754a826d6e01e642260b74c6f8
Florian MARGAINE <florian@margaine.com>
2015-10-18 21:42:58 +0200

    Adds safety checks

--- pam_nopass.c
+++ pam_nopass.c
@@ -1,13 +1,26 @@
 #include <security/pam_modules.h>
+#include <stdlib.h>
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-	return PAM_SUCCESS;
-}
+	const char *user;
+	int retval;
+	retval = pam_get_user(pamh, &user, NULL);
+	if (retval != PAM_SUCCESS)
+	{
+		return retval;
+	}
 
-PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
 	return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
+	const char *user;
+	int retval;
+	retval = pam_get_user(pamh, &user, NULL);
+	if (retval != PAM_SUCCESS)
+	{
+		return retval;
+	}
+
 	return PAM_SUCCESS;
 }
