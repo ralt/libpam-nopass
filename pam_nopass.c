2ceed6a0361b863f71561b26f704d9577ad27c18
Florian MARGAINE <florian@margaine.com>
2015-10-18 22:21:58 +0200

    Use a C macro to make things simpler!

--- pam_nopass.c
+++ pam_nopass.c
@@ -1,26 +1,20 @@
 #include <security/pam_modules.h>
 #include <stdlib.h>
 
-PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-	const char *user;
-	int retval;
-	retval = pam_get_user(pamh, &user, NULL);
-	if (retval != PAM_SUCCESS)
-	{
-		return retval;
-	}
-
+#define SUCCESS() const char *user; \
+	int retval; \
+	retval = pam_get_user(pamh, &user, NULL); \
+	if (retval != PAM_SUCCESS) \
+	{ \
+		return retval; \
+	} \
 	return PAM_SUCCESS;
+
+
+PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
+	SUCCESS()
 }
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-	const char *user;
-	int retval;
-	retval = pam_get_user(pamh, &user, NULL);
-	if (retval != PAM_SUCCESS)
-	{
-		return retval;
-	}
-
-	return PAM_SUCCESS;
+	SUCCESS()
 }
